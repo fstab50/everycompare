0b7433e91daeceb4a62ce321bebdb4d6216fa866
Michał Woźniczak <michal.wozniczak@gmail.com>
2019-05-28 21:53:03 +0200

    will use Pool.map if --count parameter is set
    Resolves #7

--- everycompare/cli.py
+++ everycompare/cli.py
@@ -1,13 +1,25 @@
 import argparse
 import sys
 
+from multiprocessing import Pool
+
 from core import compare
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dir_path', help='Directory to analyze')
+parser.add_argument('-c', '--count', type=int, default=1, help='How many comparisons to execute in parallel (default 1)')
 
 if __name__ == "__main__":
     params = vars(parser.parse_args(sys.argv[1:]))
-    
-    out = sorted(compare(params['dir_path'], True))
+
+    kwargs = {
+        'path': params['dir_path'],
+        'only_text': True
+    }
+
+    if params['count'] > 1:
+        p = Pool(params['count'])
+        kwargs['mapping_function'] = p.map
+
+    out = sorted(compare(**kwargs))
     [print(x) for x in out]
