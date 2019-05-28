122b25588f6e624567efb9bab35049373c7ac585
Michał Woźniczak <michal.wozniczak@gmail.com>
2019-05-28 21:37:52 +0200

    Added a FileMeta structure, for file-related data
    Related to issue #6

--- everycompare/structures.py
+++ everycompare/structures.py
@@ -1,3 +1,4 @@
 from collections import namedtuple
 
 ComparisonResult = namedtuple('ComparisonResult', ('difference', 'paths', 'method'))
+FileMeta = namedtuple('FileMeta', ('path', 'is_binary', 'contents', 'size', 'text_length'))
